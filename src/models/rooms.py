ea47d7f42dc8e5fb45d232c48b22ae228a8d69a1
marikkaratist <nebose@mail.ru>
2024-10-13 20:36:51 +0700

    Решение задания №3: миграции с применением настроек alembic.ini и применение библиотеки Black

--- src/models/rooms.py
+++ src/models/rooms.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import ForeignKey
 
 from src.database import Base
 
 
-class HotelsORM(Base):
-    __tablename__ = "hotels"
+class RoomsORM(Base):
+    __tablename__ = "rooms"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(100))
-    location: Mapped[str]
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    title: Mapped[str]
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
