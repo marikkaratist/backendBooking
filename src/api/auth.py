07a95de0582d81b8d652db450f237e7b10540b9c
Mark Seleznyov <nebose@mail.ru>
2024-10-23 06:47:02 +0800

    Получение пользователя из cookie. Вынесение аутентификации в Dependecies. Ручка на выход из системы.

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Body, HTTPException, Response, Request
+from fastapi import APIRouter, Body, HTTPException, Response
+from starlette.responses import RedirectResponse
+
+from src.api.dependencies import UserIdDep
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd
@@ -50,11 +53,16 @@ async def login_user(data: UserRequestAdd, response: Response):
         return {"access_token": access_token}
 
 
-@router.get("/only_auth")
-async def only_auth(
-        request: Request
+@router.get("/logout")
+async def logout(response: Response):
+    response.delete_cookie("access_token")
+    return {"status": "OK"}
+
+
+@router.get("/me")
+async def me(
+        user_id: UserIdDep
 ):
-    access_token = request.cookies.get("access_token")
-    if access_token is None:
-        return None
-    return {"access_token": access_token}
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+    return user
