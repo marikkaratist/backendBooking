479bbba4e62647d3e293c80118412d3c9159e292
marikkaratist <nebose@mail.ru>
2024-11-05 18:14:48 +0700

    Реализация CRUD запросов дла сущности Rooms

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter, Body
 
 from src.database import async_session_maker
 from src.repositories.rooms import RoomsRepository
-from src.schemas.rooms import RoomAdd
+from src.schemas.rooms import RoomAdd, RoomPATCH
 
 router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 
 @router.get("/{hotel_id}/rooms")
-async def get_rooms():
+async def get_rooms(hotel_id: int):
     async with async_session_maker() as session:
-        return await RoomsRepository(session).get_all()
+        return await RoomsRepository(session).get_all(hotel_id=hotel_id)
 
 
 @router.get("/{hotel_id}/rooms/{room_id}")
@@ -19,7 +19,7 @@ async def get_room(room_id: int):
         return await RoomsRepository(session).get_one_or_none(id=room_id)
 
 
-@router.post("/")
+@router.post("/{hotel_id}/")
 async def create_room(room_data: RoomAdd = Body(openapi_examples={
     "1": {
         "summary": "Basic", "value": {
@@ -34,7 +34,7 @@ async def create_room(room_data: RoomAdd = Body(openapi_examples={
         "summary": "Deluxe", "value": {
             "hotel_id": "2",
             "title": "Deluxe room",
-            "description": "Супер комфортный роскошный отель с двумя санузлами",
+            "description": "Супер комфортный роскошный номер с двумя санузлами",
             "price": 1500,
             "quantity": 4
         }
@@ -45,3 +45,47 @@ async def create_room(room_data: RoomAdd = Body(openapi_examples={
         room = await RoomsRepository(session).add(room_data)
         await session.commit()
         return {"status": 201, "data": room}
+
+
+@router.put("/{hotel_id}/rooms/{room_id}")
+async def update_room(room_id: int, room_data: RoomAdd = Body(openapi_examples={
+    "1": {
+        "summary": "Basic", "value": {
+            "hotel_id": "1",
+            "title": "Basic room",
+            "description": "Лайтовый номер по скидке",
+            "price": 250,
+            "quantity": 3
+        }
+    },
+    "2": {
+        "summary": "Deluxe", "value": {
+            "hotel_id": "2",
+            "title": "Deluxe room",
+            "description": "Достаточно дороговатый номер по сравнению даже с дорогими номерами",
+            "price": 2500,
+            "quantity": 6
+        }
+    }
+})
+                      ):
+    async with async_session_maker() as session:
+        await RoomsRepository(session).edit(room_data, id=room_id)
+        await session.commit()
+    return {"status": 204}
+
+
+@router.patch("/{hotel_id}/rooms/{room_id}")
+async def update_room(room_id: int, room_data: RoomPATCH):
+    async with async_session_maker() as session:
+        await RoomsRepository(session).edit(room_data, exclude_unset=True, id=room_id)
+        await session.commit()
+    return {"status": 204}
+
+
+@router.delete("/{hotel_id}/rooms/{room_id}")
+async def delete_room(room_id: int):
+    async with async_session_maker() as session:
+        await RoomsRepository(session).delete(id=room_id)
+        await session.commit()
+    return {"status": 204}
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from sqlalchemy import select
+
 from src.models.rooms import RoomsORM
 from src.repositories.base import BaseRepository
 from src.schemas.rooms import Room
@@ -6,3 +10,8 @@ from src.schemas.rooms import Room
 class RoomsRepository(BaseRepository):
     model = RoomsORM
     schema = Room
+    async def get_all(self, hotel_id: int) -> List[Room]:
+        query = select(RoomsORM).filter_by(hotel_id=hotel_id)
+
+        result = await self.session.execute(query)
+        return [Room.model_validate(room, from_attributes=True) for room in result.scalars().all()]
