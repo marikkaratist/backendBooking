d928c60c81092e48111a08cd448fa1fa64ee5376
marikkaratist <nebose@mail.ru>
2024-11-07 16:02:34 +0700

    Добавление модели bookings, применение миграций

---
+++ src/migrations/versions/2024_11_07_1546-8a84e279f980_add_bookings.py
@@ -0,0 +1,43 @@
+"""Add bookings 
+
+Revision ID: 8a84e279f980
+Revises: 9395ced823cd
+Create Date: 2024-11-07 15:46:02.979905
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "8a84e279f980"
+down_revision: Union[str, None] = "9395ced823cd"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "bookings",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("date_from", sa.Date(), nullable=False),
+        sa.Column("date_to", sa.Date(), nullable=False),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("bookings")
