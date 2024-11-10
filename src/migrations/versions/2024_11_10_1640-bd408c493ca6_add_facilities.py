eb9c1b5fb32673822f2bef0dc1b86794b33cec1d
marikkaratist <nebose@mail.ru>
2024-11-10 16:59:51 +0700

    Добавление миграции модели facilities

---
+++ src/migrations/versions/2024_11_10_1640-bd408c493ca6_add_facilities.py
@@ -0,0 +1,47 @@
+"""add facilities
+
+Revision ID: bd408c493ca6
+Revises: 8a84e279f980
+Create Date: 2024-11-10 16:40:28.863762
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "bd408c493ca6"
+down_revision: Union[str, None] = "8a84e279f980"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("rooms_facilities")
+    op.drop_table("facilities")
