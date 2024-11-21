2d14ef689eb8c54b12774da479f358d2ab09e237
marikkaratist <nebose@mail.ru>
2024-11-21 16:26:24 +0700

    Создание тестового пользователя через API

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import pytest
+from httpx import AsyncClient
 
 from src.config import settings
 from src.database import Base, engine_null_pool
+from src.main import app
 from src.models import *
+
 
 @pytest.fixture(scope="session", autouse=True)
 def check_test_mode():
@@ -10,7 +13,19 @@ def check_test_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_test_mode):
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(app=app, base_url="http://test123") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "kot@pes.com",
+                "password": "12345"
+            }
+        )
