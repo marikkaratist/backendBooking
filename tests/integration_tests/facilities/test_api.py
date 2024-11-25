8c33e6b92cc6f7769a714e06097797ab8b17f05d
marikkaratist <nebose@mail.ru>
2024-11-25 18:45:45 +0700

    Написание дополнительных assert в api_test на получение и добавление удобств

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -3,12 +3,18 @@
 async def test_get_facilities(ac):
     response = await ac.get("/facilities")
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
-async def test_post_facility(ac):
+async def test_post_facilities(ac):
+    facility_title = "Душ"
     response = await ac.post(
         "/facilities",
         json={
-            "title": "Душ"
+            "title": facility_title
         }
     )
+    res = response.json()
     assert response.status_code == 200
+    assert res["data"]["title"] == facility_title
+    assert "data" in res
+    assert isinstance(res, dict)
